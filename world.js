812fd370fdb5d14154da1b6b3e9c120922483b4d
Renaldo Pringle <rapchristoff@gmail.com>
2014-11-20 16:55:47 -0500

    Exercise 4 Completed

--- world.js
+++ world.js
@@ -2,7 +2,40 @@ window.onload = function() {
 	var button = document.getElementById("lookup");
 	var result = document.getElementById("result");
 	var term = document.getElementById("term");
-	button.addEventListener("click", function() {
+    //var res = document.;
+	/*button.addEventListener("click", function() {
 		result.innerHTML += "<div>" + term.value + "</div>";
-	});
+	});*/
+    
+    
+    function successFunction(res) {
+        //alert(res.responseText);
+        result.innerHTML = res.responseText;
+    }
+    
+    /**
+    *   adds eventlistener to the button to display the information about a country as an alert
+    *
+    */
+    button.addEventListener("click", function() {
+        /*new Ajax.Updater(
+            result,
+            "world.php",
+            {
+                parameters : {
+                    lookup: term.value
+                },
+                method: "get"
+            }
+        );*/
+        new Ajax.Request("world.php", {
+          parameters : {
+              //lookup: term.value,               //CHECK THIS!!!
+              all: term.value,
+              format: "xml"
+          },
+          onSuccess : successFunction,
+          method : "get"
+        });
+    });
 }
--- world.php
+++ world.php
@@ -6,15 +6,31 @@ mysql_connect(
 mysql_select_db("world");
 
 
-$LOOKUP = $_REQUEST['lookup'];
+//$LOOKUP = $_REQUEST['lookup'];
+$ALL = $_REQUEST['all'];
+$FORMAT = $_REQUEST['format'];
 
+/*
 # execute a SQL query on the database
 $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
-print $results;
+//print $results;
 # loop through each country
 while ($row = mysql_fetch_array($results)) {
   ?>
   <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
   <?php
+}
+*/
+
+if ($ALL == "true") {
+    $results = mysql_query("SELECT * FROM countries");
+    print $results;
+} elseif ($ALL != "") {
+    $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$ALL%';");
+}
+while ($row = mysql_fetch_array($results)) {
+    ?>
+    <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
+    <?php
 }
 ?>
